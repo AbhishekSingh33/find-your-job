84a1e2f1055a45d573f2160a6384953330359e14
AbhishekSingh33 <abhishek12221995@gmail.com>
2026-01-18 15:24:32 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
     credentials:true
 }
 
